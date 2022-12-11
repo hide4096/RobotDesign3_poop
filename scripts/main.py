9ab9c36900121ec76c106805d30fcd99c7c2e658
hide4096 <asouhide2002@gmail.com>
2022-12-12 07:38:31 +0900

    [update]動かせた

--- scripts/main.py
+++ scripts/main.py
@@ -8,13 +8,24 @@ import actionlib
 import geometry_msgs.msg
 import rosnode
 from tf import transformations
-from tf.transformations import quaternion_from_euler
+from tf.transformations import quaternion_from_euler, euler_from_quaternion
 import math
 from geometry_msgs.msg import Point,Pose
 from control_msgs.msg import (
     GripperCommandAction,
     GripperCommandGoal
 )
+
+def yaw_of(object_orientation):
+    eular = euler_from_quaternion(
+        (object_orientation.x,object_orientation.y,object_orientation.z,
+        object_orientation.w)
+    )
+
+    return eular[2]
+
+def deg2rad(degree):
+    return degree * (math.pi/180.0)
 
 def main():
     arm = moveit_commander.MoveGroupCommander("arm")
@@ -28,14 +39,37 @@ def main():
     rospy.sleep(1.0)
 
     while True:
+        # ハンドを開く
         gripper_goal.command.position = 1.2
         gripper.send_goal(gripper_goal)
         gripper.wait_for_result(rospy.Duration(1.0))
 
+        # ホーム姿勢にする
         arm.set_named_target("home")
         arm.go()
-
         rospy.sleep(1.0)
+
+        print("Start")
+
+        for i in range(180):
+
+            target_pose = Pose()
+            target_pose.position.z = 0.3
+
+
+            q = quaternion_from_euler(deg2rad(160),0.0,deg2rad(i))
+            target_pose.orientation.x = q[0]
+            target_pose.orientation.y = q[1]
+            target_pose.orientation.z = q[2]
+            target_pose.orientation.w = q[3]
+
+            arm.set_pose_target(target_pose)
+
+            if arm.go() is False:
+                print("Failed")
+                while True:
+                    rospy.sleep(1.0)
+            rospy.sleep(1.0)
 
 if __name__ == '__main__':
     rospy.init_node("Move_arm_example")
